1688ca1283f544cbe38520d47aa0ada22de6a6c9
futpib <futpib@gmail.com>
2019-03-29 19:02:20 +0300

    Switch to "@futpib/dagre" fork

--- karma.core.conf.js
+++ karma.core.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
       'node_modules/lodash/lodash.js',
       'node_modules/d3/build/d3.js',
       'node_modules/graphlib/dist/graphlib.core.js',
-      'node_modules/dagre/dist/dagre.core.js',
+      'node_modules/@futpib/dagre/dist/dagre.core.js',
       'build/dist/dagre-d3.core.js',
 
       'node_modules/chai/chai.js',
